dd07f333a5b44578a18e69b6908f0cf9429e5dfb
leihuang <leihuang007@qq.com>
2021-02-05 17:38:57 +0800

    增加自定义cookie及user-agent能力。

--- my_facebook_withoutapi.py
+++ my_facebook_withoutapi.py
@@ -6,6 +6,8 @@ import facebook_scraper
 import json
 from requests.cookies import RequestsCookieJar
 import os
+import time
+
 
 class DateEncoder(json.JSONEncoder):
     """json.dumps默认无法将datetime类型的属性进行序列化，因此需要额外增加解析器"""
@@ -18,16 +20,32 @@ class DateEncoder(json.JSONEncoder):
 
 
 def scrap_without_cookie():
-    group_list = [891546924927251, ]
-
-    for index in range(100):
-        with open(os.path.join('haha', f"{index}.json"), 'w') as file_handler:
-            print(f"Saving file {index} with post_id={index}")
-            file_handler.write(json.dumps('{"haha":"lala"}', indent=1, cls=DateEncoder))
-    # for index, post in enumerate(facebook_scraper.get_posts(group=1288475234841795, pages=1)):
-    #     with open(f'COVID-19-FACEBOOK\\{post["post_id"]}.json', 'w') as file_handler:
-    #         print(f"Saving file{index} with post_id={post['post_id']}")
-    #         file_handler.write(json.dumps(post, indent=1, cls=DateEncoder))
+    group_list = ['COVID19Resources',
+                  373920943948661, 1288475234841795, 256933822441031, 1047666978949055, 'solidaritycandle',
+                  'VancouverIslandEmergencyPreparedness', 1288475234841795, 891546924927251, ]
+    my_scraper = facebook_scraper._scraper
+    my_scraper.user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/88.0.4324.146 Safari/537.36 Edg/88.0.705.62"
+    )
+    my_scraper.cookie = ("sb=ZSu7X0nIEY9Mb2gb5-_vzxTe; "
+                         "datr=ZSu7X9aILwqfhmrzhEuXDtqd; "
+                         "_fbp=fb.1.1610674279287.2026618001; "
+                         "locale=en_US; wd=1440x796; "
+                         "c_user=100009018647083; "
+                         "spin=r.1003264875_b.trunk_t.1612428646_s.1_v.2_; "
+                         "presence=EDvF3EtimeF1612489368EuserFA21B09018647083A2EstateFDutF0CEchF_7bCC; "
+                         "xs=19%3Anx2rLxT33gUAlw%3A2%3A1612428642%3A16804%3A8668%3A%3AAcV_cf-bvvG3dcYEEU-ewMBnuir8pVW7WOS8aAFHBA; "
+                         "fr=1yiV9hm7zUnVWlv1j.AWXXogAarLNdG_u00cJlBCjZ8Dk.Bfuytl.KB.F_9.0.0.BgHQBp.AWWcpIXb_mg")
+    for group_id in group_list:
+        print(f'Processing Group[{group_id}]')
+        for index, post in enumerate(facebook_scraper.get_posts(group=group_id, pages=500)):
+            with open(os.path.join('COVID-19-FACEBOOK', f'{post["post_id"]}.json'), 'w') as file_handler:
+                print(f"Saving file{index} with post_id={post['post_id']}")
+                file_handler.write(json.dumps(post, indent=1, cls=DateEncoder))
+        print(f'Done Group[{group_id}]')
+        time.sleep(15)
 
 
 def scrap_with_cookie():
